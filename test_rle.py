9cf6e843eeb865eeaf90e4023bdccd1325e74535
Maurizio Tomasi <ziotom78@gmail.com>
2015-10-01 09:09:22 +0200

    Add test for RLE decompression

--- test_rle.py
+++ test_rle.py
@@ -7,3 +7,11 @@ def test_compression():
         compressed = pypolycomp.rle_compress(np.array([1, 1, 1, 2, 3], dtype=cur_type))
         assert np.all(compressed == np.array([3, 1, 1, 2, 1, 3], dtype=cur_type))
 
+def test_decompression():
+    for cur_type in (np.int8, np.int16, np.int32, np.int64,
+                     np.uint8, np.uint16, np.uint32, np.uint64):
+        input_values = np.array(np.random.randint(100, size=1000),
+                                dtype=cur_type)
+        compressed = pypolycomp.rle_compress(input_values)
+        output_values = pypolycomp.rle_decompress(compressed)
+        assert np.all(input_values == output_values)
