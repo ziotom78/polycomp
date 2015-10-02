86ded09e495707d9a1df1643ca189e81fbdbf671
Maurizio Tomasi <ziotom78@gmail.com>
2015-10-02 18:33:39 +0200

    Switch from "distutils" to "setuptools"

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- mode: python -*-
 
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup
+from setuptools.extension import Extension
 from Cython.Build import cythonize
 
 modules = [Extension("pypolycomp",
